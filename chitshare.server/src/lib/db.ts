c24a1d0696ebe626eae63f8e05c600eff8bcf8bb
thorved <vedp3141@gmail.com>
2026-01-03 17:08:31 +0530

    refactor: Initialize PrismaPg adapter using a `pg.Pool` instance.

--- chitshare.server/src/lib/db.ts
+++ chitshare.server/src/lib/db.ts
@@ -1,16 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  adapter
-});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
